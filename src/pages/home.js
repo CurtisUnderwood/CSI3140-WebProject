be54d97cdaf2c846f71d7a35e539626f63625d73
Curtis Underwood <curtisund@gmail.com>
2022-05-29 15:53:16 -0400

    homepage tweaks and validation

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,58 +1,59 @@
 import React, { Component } from "react";
 import questionBox from "../icons/questionbox.svg";
 import questionBoxWhite from "../icons/questionBoxWhite.svg";
-import scoreBox from "../icons/scoreBox.svg";
+import logoBlock from "../icons/logoBlock.svg";
+import timerBlock from "../icons/timerBlock.svg";
 import "../styles.css";
 import { Footer } from "../components";
 
 export default class Home extends Component {
   render() {
     return (
-      <div className="layout">
-          <div className="banner">
-            <div className="row">
-              <div className="column">
+      <div class="layout">
+          <div class="banner">
+            <div class="row">
+              <div class="column">
                 <h1>Welcome to Trivia App!</h1>
                 <p>Get started or learn how to play.</p>
-                <a href='/trivia' className="playButton">Play Now</a>
+                <a href='/trivia' class="playButton">Play Now</a>
               </div>
-              <div className="column">
-                <img src={questionBox}></img>
+              <div class="column">
+                <img src={questionBox}/>
               </div>
             </div>
           </div>
 
-          <div className="row">
-              <div className="column">
-                <div className="banner">
+          <div class="row">
+              <div class="column">
+                <div class="banner">
                   <h1>How to Play</h1>
                   <p>Learn how to play with our players guide down below.</p>
                 </div>
               </div>
-              <div className="column">
-                <img src={questionBox}></img>
+              <div class="column">
+                <img src={logoBlock}/>
               </div>
           </div>
 
-          <div className="row">
-              <div className="column">
-                <div className="banner">
+          <div class="row">
+              <div class="column">
+                <div class="banner">
                   <h1>1. Select one of the four boxes to answer a question</h1>
                 </div>
               </div>
-              <div className="column">
-                <img src={questionBoxWhite}></img>
+              <div class="column">
+                <img src={questionBoxWhite}/>
               </div>
           </div>
 
-          <div className="row">
-              <div className="column">
-                <div className="banner">
+          <div class="row">
+              <div class="column">
+                <div class="banner">
                   <h1>2. Answer before the time runs out!</h1>
                 </div>
               </div>
-              <div className="column">
-                <img src={scoreBox}></img>
+              <div class="column">
+                <img src={timerBlock}/>
               </div>
           </div>
 
