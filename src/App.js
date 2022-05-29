5f5e3d6d63635245542a2490a18a82a718a8f0b8
Curtis Underwood <curtisund@gmail.com>
2022-05-29 16:04:13 -0400

    meta data

--- src/App.js
+++ src/App.js
@@ -6,10 +6,18 @@ import Game from "./pages/game";
 import Scores from "./pages/scores";
 import AllScores from "./pages/allscores";
 import Home from "./pages/home";
+import { Helmet } from 'react-helmet'
+import logo from './icons/logo.svg'
 
 function App() {
   return (
     <div className="App">
+      <Helmet>
+        <title>Trivia App</title>
+        <meta name="description" content="A basic trivia app built in React." />
+        <meta name="theme-color" content="#6a5be1" />
+        <link rel="icon" href={logo}></link>
+      </Helmet>
       <Header />
       <Routes>
         <Route path="/" element={<Home />}>
