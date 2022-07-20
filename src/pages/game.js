8429db82e954b20d377217ff4800c41f9cae919e
Curtis Underwood <curtisund@gmail.com>
2022-07-19 23:02:51 -0400

    trivia game

--- src/pages/game.js
+++ src/pages/game.js
@@ -1,21 +1,33 @@
-import React, { Component } from "react";
-import { FourSquare } from "../components/foursquare";
+import React, { useState } from "react";
 import { Timer } from "../components/timer";
 import "../styles.css";
+import {questions} from "../questions.js"
 
-export default class Game extends Component {
-  render() {
+export default function Game() {
+
+  const [activeQuestion, setActiveQuestion] = useState(0);
+	const [score, setScore] = useState(0);
+
+	const optionClickHandler = (correctFlag) => {
+		if (correctFlag == true) {
+			setScore(score + 1);
+		}
+
+		const nextQuestion = activeQuestion + 1;
+		if (nextQuestion < questions.length) {
+			setActiveQuestion(nextQuestion);
+		}
+	};
     return (
       <div className="game">
         <Timer />
-        <span className="game-score">Score: 1</span>
-        <span className="question">{this.props.question}</span>
-        <FourSquare />
+        <span className="game-score">Score: {score}</span>
+        <span className="question">{questions[activeQuestion].title}</span>
+        <div class="foursquare">
+          {questions[activeQuestion].options.map((answerOption) => (
+                <button style={{backgroundColor: answerOption.color}} onClick={() => optionClickHandler(answerOption.correctFlag)}>{answerOption.title}</button>
+              ))}
+        </div>
       </div>
     );
-  }
 }
-
-Game.defaultProps = {
-  question: "What is the majority of earths surface covered in?",
-};
---
+++ src/questions.js
@@ -0,0 +1,102 @@
+export const questions = [
+  {
+    title: "What geometric shape is a stop sign?",
+    options: [
+      {  title: "Square", correctFlag: false, color:'#67E383' },
+      {  title: "Triangle", correctFlag: false, color:'#F0F277' },
+      {  title: "Pentagon", correctFlag: false, color:'#FE9494' },
+      {  title: "Octagon", correctFlag: true, color:'#B9C8FF' },
+    ],
+  },
+  {
+    title: "What was the most downloaded app in 2021?",
+    options: [
+      {  title: "BeReal", correctFlag: false, color:'#67E383' },
+      {  title: "Instagram", correctFlag: false, color:'#F0F277' },
+      {  title: "Snapchat", correctFlag: false, color:'#FE9494' },
+      {  title: "TikTok", correctFlag: true, color:'#B9C8FF' },
+    ],
+  },
+  {
+    title: "What did Kylie Jenner name her baby??",
+    options: [
+      {  title: "Raini", correctFlag: false, color:'#67E383' },
+      {  title: "North", correctFlag: false, color:'#F0F277' },
+      {  title: "Moon", correctFlag: false, color:'#FE9494' },
+      {  title: "Stormi", correctFlag: true, color:'#B9C8FF' },
+    ],
+  },
+  {
+    title: "When did Whole Lotta Red by Playboi Carti get released?",
+    options: [
+      {  title: "Dec 25, 2021", correctFlag: false, color:'#67E383' },
+      {  title: "Dec 31, 2021", correctFlag: false, color:'#F0F277' },
+      {  title: "Dec 31, 2020", correctFlag: false, color:'#FE9494' },
+      {  title: "Dec 25, 2020", correctFlag: true, color:'#B9C8FF' },
+    ],
+  },
+  {
+    title: "Which artist boycotted the Grammys after being snubbed in 2021?",
+    options: [
+      {  title: "Drake", correctFlag: false, color:'#67E383' },
+      {  title: "Travis Scott", correctFlag: false, color:'#F0F277' },
+      {  title: "Post Malone", correctFlag: false, color:'#FE9494' },
+      {  title: "The Weeknd", correctFlag: true, color:'#B9C8FF' },
+    ],
+  },
+  {
+    title: "What was Olivia Rodrigos debut album titled?",
+    options: [
+      {  title: "Sweet", correctFlag: false, color:'#67E383' },
+      {  title: "Angsty", correctFlag: false, color:'#F0F277' },
+      {  title: "Bitter", correctFlag: false, color:'#FE9494' },
+      {  title: "Sour", correctFlag: true, color:'#B9C8FF' },
+    ],
+  },
+  {
+    title: "Artist Kanye West changed his name to?",
+    options: [
+      {  title: "Yeezy", correctFlag: false, color:'#67E383' },
+      {  title: "Yeezus", correctFlag: false, color:'#F0F277' },
+      {  title: "Kanye", correctFlag: false, color:'#FE9494' },
+      {  title: "Ye", correctFlag: true, color:'#B9C8FF' },
+    ],
+  },
+  {
+    title: "With a return to in-person learning, many second year professors noticed class averages ?",
+    options: [
+      {  title: "Increased", correctFlag: false, color:'#67E383' },
+      {  title: "Did not change", correctFlag: false, color:'#F0F277' },
+      {  title: "Increased Significantly", correctFlag: false, color:'#FE9494' },
+      {  title: "Decreased", correctFlag: true, color:'#B9C8FF' },
+    ],
+  },
+  {
+    title: "What programming language is commonly used in front-ends?",
+    options: [
+      {  title: "HTML", correctFlag: false, color:'#67E383' },
+      {  title: "CSS", correctFlag: false, color:'#F0F277' },
+      {  title: "PHP", correctFlag: false, color:'#FE9494' },
+      {  title: "Javascript", correctFlag: true, color:'#B9C8FF' },
+    ],
+  },
+  {
+    title: "Which prominent rapper now identifies as they/them?",
+    options: [
+      {  title: "Lil Baby", correctFlag: false, color:'#67E383' },
+      {  title: "Lil Wayne", correctFlag: false, color:'#F0F277' },
+      {  title: "Lil Durk", correctFlag: false, color:'#FE9494' },
+      {  title: "Lil Uzi Vert", correctFlag: true, color:'#B9C8FF' },
+    ],
+  },
+  {
+    title: "An obsessive fan maybe called ___ on twitter?",
+    options: [
+      {  title: "Super fan", correctFlag: false, color:'#67E383' },
+      {  title: "Big fan", correctFlag: false, color:'#F0F277' },
+      {  title: "Fan", correctFlag: false, color:'#FE9494' },
+      {  title: "Stan", correctFlag: true, color:'#B9C8FF' },
+    ],
+  },
+  
+];
